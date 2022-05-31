65a7c2f7daf46b405e50940874ce5f9ff8d34dd2
giles.tai <giles.tai@umec.com.tw>
2022-05-31 15:35:43 +0800

    添加完成確認日期

--- 合併弱掃結果/merage_excel.py
+++ 合併弱掃結果/merage_excel.py
@@ -60,7 +60,8 @@ result.columns = ['任務名稱','弱點描述', '任務執行時間','IP','Host
 result["處理步驟"] = np.nan
 result["是否可修補(Y/N)"] = np.nan
 result["是否為新弱點(Y/N)"] = "N"
-#result["OS版本"] = np.nan，然後插入到5欄位的後面
+result["完成確認日期"] = np.nan #此index為16
+#result["OS版本"] = np.nan，然後插入到特定順序欄位的後面，index從0開始
 result.insert(5, "系統管理者", np.nan)
 result.insert(6, "OS版本", np.nan)
 result.insert(1, "漏洞類型", "未定義")
